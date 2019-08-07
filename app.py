7ae765a8dcdf82c4d110664b56766568ed94b073
Bennie <brobinson88@gmail.com>
2019-08-07 12:04:40 -0400

    Migrate to Chart.js

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def top_ten_artists_json(username):
 
 @app.route('/top-ten/<username>', methods=['GET'])
 def top_ten_artists(username):
-    # artists = get_top_ten_artists(username)
     return render_template(
         'top-ten.html',
         username=username
