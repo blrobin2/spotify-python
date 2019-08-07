5e36ffe0f5750a7c4124e2dc248908dc16f50d3b
Bennie <brobinson88@gmail.com>
2019-08-07 10:50:05 -0400

    Add progress bar for command line runner

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from collections import Counter
 from pprint import pprint
+from tqdm import tqdm
 
 
 def init_spotify_api():
@@ -13,6 +14,40 @@ def init_spotify_api():
     )
 
     return spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+def get_top_ten_artists(username, show_progress_bar=False):
+    sp = init_spotify_api()
+    playlists = sp.user_playlists(username)
+
+    # get artists from playlist
+    # count each instance
+    artists = []
+    owner_playlists = [playlist for playlist in playlists.get('items') if playlist['owner']['id'] == username]
+    if show_progress_bar:
+        progress_bar = tqdm(
+            total=len(owner_playlists),
+            unit='playlist'
+        )
+
+    for playlist in owner_playlists:
+        results = sp.user_playlist(username, playlist.get('id'), fields="tracks,next")
+        tracks = results.get('tracks')
+
+        for i, item in enumerate(tracks.get('items')):
+            track = item.get('track')
+            artists.append(track['artists'][0]['name'])
+
+        while tracks.get('next'):
+            tracks = sp.next(tracks)
+            for i, item in enumerate(tracks.get('items')):
+                track = item.get('track')
+                artists.append(track['artists'][0]['name'])
+
+        if show_progress_bar:
+            progress_bar.update()
+
+    return Counter(artists).most_common(10)
 
 
 def show_owner_playlists(playlists, username):
@@ -46,26 +81,6 @@ if __name__ == '__main__':
         print("usage: python main.py [username]")
         sys.exit()
 
-    sp = init_spotify_api()
-    playlists = sp.user_playlists(username)
-
-    # get artists from playlist
-    # count each instance
-    artists = []
-    for playlist in playlists.get('items'):
-        if playlist['owner']['id'] == username:
-            results = sp.user_playlist(username, playlist.get('id'), fields="tracks,next")
-            tracks = results.get('tracks')
-
-            for i, item in enumerate(tracks.get('items')):
-                track = item.get('track')
-                artists.append(track['artists'][0]['name'])
-
-            while tracks.get('next'):
-                tracks = sp.next(tracks)
-                for i, item in enumerate(tracks.get('items')):
-                    track = item.get('track')
-                    artists.append(track['artists'][0]['name'])
-
-    count = Counter(artists).most_common(10)
-    pprint(count)
+    print("Checking artist in each playlist: ")
+    top_ten = get_top_ten_artists(username, show_progress_bar=True)
+    pprint(top_ten)
