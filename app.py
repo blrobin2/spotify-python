d63010e55730b62db69ea03a45ea9d12818f619a
Bennie <brobinson88@gmail.com>
2019-08-07 11:30:48 -0400

    Render table for data

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 from api import get_top_ten_artists
 
 app = Flask(__name__)
 
 
+@app.route('/api/top-ten/<username>', methods=['GET'])
+def top_ten_artists_json(username):
+    artists = get_top_ten_artists(username)
+    return jsonify({'data': artists})
+
+
 @app.route('/top-ten/<username>', methods=['GET'])
 def top_ten_artists(username):
-    artists = get_top_ten_artists(username)
+    # artists = get_top_ten_artists(username)
     return render_template(
         'top-ten.html',
-        username=username,
-        artists=artists
+        username=username
     )
